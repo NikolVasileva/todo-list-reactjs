ee205b310ebccb03bd611469aa82350d54fa27c6
NikolVasileva <nikol.vasileva9@gmail.com>
2025-11-07 23:37:12 +0200

    Create TodoItem.jsx and rendering list

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,9 @@
+export default function TodoItem({
+    _id,
+    title,
+    completed
+}) {
+    return(
+        <li>{title}</li>
+    )
+}
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,23 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import TodoItem from "./TodoItem";
 
 export default function TodoList() {
+    const [todos, setTodos] = useState([]);
+
     useEffect(() => {
         fetch("http://localhost:3030/jsonstore/todos")
           .then(response => response.json())
           .then(data => {
-            console.log(data)
+            setTodos(Object.values(data))
           })
           .catch(err => alert(err.message))
-    })
+    },[])
 
     return(
-        <div></div>
+        <ul>{todos.map(todo => <TodoItem 
+            title={todo.title}
+            key={todo._id}
+            />)}
+            </ul>
     )
 }
